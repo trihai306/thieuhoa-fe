d1d58c9df7ad6d690f4db4149827e39b05e814b3
Phạm Văn Nguyên <npv2k1@gmail.com>
2024-07-13 09:49:02 +0700

    update blog banner

---
+++ src/components/BlogBanner/index.tsx
@@ -0,0 +1,87 @@
+import { useCallback, useRef } from 'react';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
+
+import Image from '@/components/Image';
+
+import 'swiper/css/navigation';
+
+import 'swiper/css';
+
+const banners = [
+  {
+    href: 'https://thieuhoa.com.vn/vay-dam-trung-nien?utm_source=blogbanner&utm_medium=articleads&utm_campaign=daily',
+    src: '/v2/img/banner-small/banner-small-1.jpg',
+    alt: '',
+  },
+  {
+    href: 'https://thieuhoa.com.vn/ao-trung-nien?utm_source=blogbanner&utm_medium=articleads&utm_campaign=daily',
+    src: '/v2/img/banner-small/banner-small-2.jpg',
+    alt: '',
+  },
+  {
+    href: 'https://thieuhoa.com.vn/dam-du-tiec?utm_source=blogbanner&utm_medium=articleads&utm_campaign=daily',
+    src: '/v2/img/banner-small/banner-small-5.jpg',
+    alt: '',
+  },
+  {
+    href: 'https://thieuhoa.com.vn/tui-xach-nu?utm_source=blogbanner&utm_medium=articleads&utm_campaign=daily',
+    src: '/v2/img/banner-small/banner-small-4.jpg',
+    alt: '',
+  },
+  {
+    href: 'https://thieuhoa.com.vn/giay-trung-nien?utm_source=blogbanner&utm_medium=articleads&utm_campaign=daily',
+    src: '/v2/img/banner-small/banner-small-3.jpg',
+    alt: '',
+  },
+];
+
+const BlogBanner = () => {
+  const sliderRef = useRef<SwiperRef>(null);
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current?.swiper?.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current?.swiper?.slideNext();
+  }, []);
+  return (
+    <div className="banner tw-h-[300px]">
+      <Swiper
+        ref={sliderRef}
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper: any) => console.log(swiper)}
+      >
+        {banners.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <a
+              className="tw-relative tw-inline-block tw-h-[300px] tw-w-full"
+              href={banner.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image fill src={banner.src} alt={banner.alt} />
+            </a>
+          </SwiperSlide>
+        ))}
+        <button
+          className="tw-absolute tw-left-0 tw-top-[calc(50%-45px/2)] tw-z-10"
+          onClick={handlePrev}
+        >
+          <Image src={`/v2/img/icon-left.png`} width={45} height={45} alt={''} />
+        </button>
+        <button
+          className="tw-absolute tw-right-0 tw-top-[calc(50%-45px/2)] tw-z-10"
+          onClick={handleNext}
+        >
+          <Image src={`/v2/img/icon-right.png`} width={45} height={45} alt={''} />
+        </button>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BlogBanner;
---
+++ src/components/BlogFeedback/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const BlogFeedback = () => {
+  return (
+    <div className="blog-feedback">
+      <div className="flex-box">
+        <p className="question flex-full flex-middle">Bạn có hài lòng bài viết này?</p>
+        <div className="feedback-icon">
+          <label className="positive">
+            <svg
+              width="2.5em"
+              height="2.5em"
+              viewBox="0 0 16 16"
+              className="bi bi-emoji-smile"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683z"
+              />
+              <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z" />
+            </svg>
+            <span>Hài lòng</span>{' '}
+          </label>
+        </div>
+        <div className="feedback-icon">
+          <label className="positive">
+            <svg
+              width="2.5em"
+              height="2.5em"
+              viewBox="0 0 16 16"
+              className="bi bi-emoji-frown"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683z"
+              />
+              <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z" />
+            </svg>
+            <span>Không hài lòng</span>{' '}
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogFeedback;
